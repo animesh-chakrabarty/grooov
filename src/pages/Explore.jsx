9e0c9b44f5530d10dcd0e586c9c2d12a4631062a
Animesh Chakrabarty <animesh_chakrabarty@outlook.com>
2023-10-08 15:45:42 +0530

    commit

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -51,7 +51,7 @@ const Explore = () => {
       {/* Top Div */}
       <div>
         {/* Logo for mobile devices */}
-        <div className="md:hidden pl-2 pr-5 pt-1  flex justify-between items-center">
+        <div className="md:hidden pl-2 pr-5 flex justify-between items-center">
           <img src={logo2} alt="" className="h-[50px] w-[150px]" />
           <button
             onClick={() => {
