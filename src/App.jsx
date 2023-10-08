d51980c352cf4426511e7ce57c408c01758eb247
Animesh Chakrabarty <animesh_chakrabarty@outlook.com>
2023-10-08 16:21:24 +0530

    mobile navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Explore from "./pages/Explore";
 import Search from "./pages/Search";
 import Albums from "./pages/Albums";
 import Trendings from "./pages/Trendings";
 import Charts from "./pages/Charts";
 import Favourites from "./pages/Favourites";
+
+import { AiFillHome } from "react-icons/ai";
+import { AiOutlineSearch } from "react-icons/ai";
+import { LuLibrary } from "react-icons/lu";
 
 import Sidebar from "./components/Sidebar";
 import AudioPlayer from "./components/AudioPlayer";
@@ -19,7 +23,29 @@ const App = () => {
         <AudioPlayer />
       </div>
       {/* Body */}
-      <div className="w-[110%] bg-[#FFFFFF] py-3 lg:px-5 max-lg:px-0 md:rounded-3xl lg:mx-5 overflow-auto no-scrollbar md:m-[15px]">
+      {/* Mobile Nav */}
+      <div className="md:hidden h-[70px] w-full mobile-nav-bg bottom-0 absolute flex justify-between items-center text-white px-4 text-[17px] pt-1">
+        <Link className="flex flex-col justify-center items-center " to="/">
+          <AiFillHome size={25} />
+          Home
+        </Link>
+        <Link
+          className="flex flex-col justify-center items-center "
+          to="/search"
+        >
+          <AiOutlineSearch size={25} />
+          Search
+        </Link>
+        <Link
+          className="flex flex-col justify-center items-center "
+          to="favourites"
+        >
+          <LuLibrary size={25} />
+          Library
+        </Link>
+      </div>
+      {/* Routes */}
+      <div className="w-[110%] bg-[#FFFFFF] py-3 lg:px-5 max-lg:px-0 md:rounded-3xl lg:mx-5 overflow-auto no-scrollbar md:m-[15px] h-[calc(100%-70px)]">
         <Routes>
           <Route path="/" element={<Explore />}></Route>
           <Route path="/search" element={<Search />}></Route>
